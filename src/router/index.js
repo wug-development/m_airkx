0b7af016ff41313601cd41469b50de16e1b9d539
wuguang <guangwu@roseonly.com>
2020-09-25 18:20:55 +0800

    更改打包文件 不打包第三方文件

--- src/router/index.js
+++ src/router/index.js
@@ -1,126 +1,145 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Index from '@/components/Index'
-import Visa from '@/components/Visa'
-import Safe from '@/components/Safe'
-import Hotel from '@/components/Hotel'
-import VisaSearch from '@/components/VisaSearch'
-import FlightList from '@/components/FlightList'
-import ReturnFlight from '@/components/ReturnFlight'
-import FlightInfo from '@/components/FlightInfo'
-import My from '@/components/My'
-import Service from '@/components/Service'
-import About from '@/components/About'
-import Complaint from '@/components/Complaint'
-import OrderInfo from '@/components/OrderInfo'
-import AddLinkman from '@/components/AddLinkman'
-import AddPerson from '@/components/AddPerson'
-import FirstCabin from '@/components/FirstCabin'
-import QYYJ from '@/components/QYYJ'
-import ZFCG from '@/components/ZFCG'
-import SearchResult from '@/components/SearchResult'
 
 Vue.use(Router)
 
 export default new Router({
-  scrollBehavior(to,from,savedPosition){
-    return {x:0,y:0}
-  },
-  routes: [
-    {
-      path: '/',
-      name: 'Index',
-      component: Index
+    scrollBehavior(to, from, savedPosition) {
+        return { x: 0, y: 0 }
     },
-    {
-      path: '/visa',
-      name: 'Visa',
-      component: Visa
-    },
-    {
-      path: '/safe',
-      name: 'Safe',
-      component: Safe
-    },
-    {
-      path: '/hotel',
-      name: 'Hotel',
-      component: Hotel
-    },
-    {
-      path: '/visasearch',
-      name: 'VisaSearch',
-      component: VisaSearch
-    },
-    {
-      path: '/flightlist',
-      name: 'FlightList',
-      component: FlightList
-    },
-    {
-      path: '/returnflight',
-      name: 'ReturnFlight',
-      component: ReturnFlight
-    },
-    {
-      path: '/flightinfo',
-      name: 'FlightInfo',
-      component: FlightInfo
-    },
-    {
-      path: '/my',
-      name: 'My',
-      component: My
-    },
-    {
-      path: '/service',
-      name: 'Service',
-      component: Service
-    },
-    {
-      path: '/about',
-      name: 'About',
-      component: About
-    },
-    {
-      path: '/complaint',
-      name: 'Complaint',
-      component: Complaint
-    },
-    {
-      path: '/addlinkman',
-      name: 'AddLinkman',
-      component: AddLinkman
-    },
-    {
-      path: '/addperson',
-      name: 'AddPerson',
-      component: AddPerson
-    },
-    {
-      path: '/orderinfo',
-      name: 'OrderInfo',
-      component: OrderInfo
-    },
-    {
-      path: '/firstcabin',
-      name: 'FirstCabin',
-      component: FirstCabin
-    },
-    {
-      path: '/qyyj',
-      name: 'QYYJ',
-      component: QYYJ
-    },
-    {
-      path: '/zfcg',
-      name: 'ZFCG',
-      component: ZFCG
-    },
-    {
-      path: '/searchresult',
-      name: 'SearchResult',
-      component: SearchResult
-    }
-  ]
+    routes: [
+        {
+            path: '/',
+            name: 'Index',
+            component(resolve) {
+                require(['@/components/Index'], resolve)
+            }
+        },
+        {
+            path: '/visa',
+            name: 'Visa',
+            component(resolve) {
+                require(['@/components/Visa'], resolve)
+            }
+        },
+        {
+            path: '/safe',
+            name: 'Safe',
+            component(resolve) {
+                require(['@/components/Safe'], resolve)
+            }
+        },
+        {
+            path: '/hotel',
+            name: 'Hotel',
+            component(resolve) {
+                require(['@/components/Hotel'], resolve)
+            }
+        },
+        {
+            path: '/visasearch',
+            name: 'VisaSearch',
+            component(resolve) {
+                require(['@/components/VisaSearch'], resolve)
+            }
+        },
+        {
+            path: '/flightlist',
+            name: 'FlightList',
+            component(resolve) {
+                require(['@/components/FlightList'], resolve)
+            }
+        },
+        {
+            path: '/returnflight',
+            name: 'ReturnFlight',
+            component(resolve) {
+                require(['@/components/ReturnFlight'], resolve)
+            }
+        },
+        {
+            path: '/flightinfo',
+            name: 'FlightInfo',
+            component(resolve) {
+                require(['@/components/FlightInfo'], resolve)
+            }
+        },
+        {
+            path: '/my',
+            name: 'My',
+            component(resolve) {
+                require(['@/components/My'], resolve)
+            }
+        },
+        {
+            path: '/service',
+            name: 'Service',
+            component(resolve) {
+                require(['@/components/Service'], resolve)
+            }
+        },
+        {
+            path: '/about',
+            name: 'About',
+            component(resolve) {
+                require(['@/components/About'], resolve)
+            }
+        },
+        {
+            path: '/complaint',
+            name: 'Complaint',
+            component(resolve) {
+                require(['@/components/Complaint'], resolve)
+            }
+        },
+        {
+            path: '/addlinkman',
+            name: 'AddLinkman',
+            component(resolve) {
+                require(['@/components/AddLinkman'], resolve)
+            }
+        },
+        {
+            path: '/addperson',
+            name: 'AddPerson',
+            component(resolve) {
+                require(['@/components/AddPerson'], resolve)
+            }
+        },
+        {
+            path: '/orderinfo',
+            name: 'OrderInfo',
+            component(resolve) {
+                require(['@/components/OrderInfo'], resolve)
+            }
+        },
+        {
+            path: '/firstcabin',
+            name: 'FirstCabin',
+            component(resolve) {
+                require(['@/components/FirstCabin'], resolve)
+            }
+        },
+        {
+            path: '/qyyj',
+            name: 'QYYJ',
+            component(resolve) {
+                require(['@/components/QYYJ'], resolve)
+            }
+        },
+        {
+            path: '/zfcg',
+            name: 'ZFCG',
+            component(resolve) {
+                require(['@/components/ZFCG'], resolve)
+            }
+        },
+        {
+            path: '/searchresult',
+            name: 'SearchResult',
+            component(resolve) {
+                require(['@/components/SearchResult'], resolve)
+            }
+        }
+    ]
 })
